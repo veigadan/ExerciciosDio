413a4f256b317e751acacccb313dbab71173880b
Daniel Veiga <danielfontesveiga@gmail.com>
2026-02-25 11:33:49 -0300

    Exercício 5

---
+++ src/Exercicios/QuantidadeSalario.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+//5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse
+//
+//usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20).
+
+public class QuantidadeSalario {
+    static void main() {
+            double salarioMinimo = 1293.20;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor do seu salário: ");
+        if (scanner.hasNextDouble()) {
+            double salarioUsuario = scanner.nextDouble();
+            double quantidadeSalario = salarioUsuario / salarioMinimo;
+
+            System.out.printf("O usuário ganha %.2f salários mínimos.%n", quantidadeSalario);
+        } else {
+            System.out.println("Erro: Por favor, digite um valor numérico válido.");
+        }
+        scanner.close();
+
+    }
+}
